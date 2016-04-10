69fed33dd5d4195bcbdc51e95c3a86240d146a67
Raffaele Salmaso <raffaele@salmaso.org>
2016-04-10 23:11:08 +0200

    update setup.py info

--- setup.py
+++ setup.py
@@ -8,23 +8,24 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='django-coupons',
+    name="django-fluo-coupons",
     version=coupons.__version__,
-    description='A reuseable Django application for coupon gereration and handling.',
-    long_description=read('README.md'),
-    license=read('LICENSE'),
-    author='byteweaver',
-    author_email='contact@byteweaver.net',
-    url='https://github.com/byteweaver/django-coupons',
+    description="A reuseable Django application for coupon gereration and handling.",
+    long_description=read("README.md"),
+    license=read("LICENSE"),
+    author="Raffaele Salmaso",
+    author_email="raffaele@salmaso.org",
+    url="https://bitbucket.org/rsalmaso/django-fluo-coupons",
     include_package_data=True,
     packages=find_packages(),
     classifiers=[
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ]
 )
