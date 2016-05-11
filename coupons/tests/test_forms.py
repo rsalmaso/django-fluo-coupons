5290d51a7fdbf1321054f254953667fb567f720d
Raffaele Salmaso <raffaele@salmaso.org>
2016-05-11 15:42:29 +0200

    tests: get User model with get_user_model

--- coupons/tests/test_forms.py
+++ coupons/tests/test_forms.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 from django.utils import timezone
 from django.test import TestCase
 
--- coupons/tests/test_use_cases.py
+++ coupons/tests/test_use_cases.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 from django.test import TestCase
 
 from coupons.forms import CouponForm
